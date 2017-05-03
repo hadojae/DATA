6a4615ac510df42ce035ae234340385a59b0e3cb
hadojae <jae.williams@gmail.com>
2017-05-02 22:02:20 -0500

    initial

--- bucklegripper/install_bucklegripper.sh
+++ bucklegripper/install_bucklegripper.sh
@@ -6,7 +6,7 @@ sudo apt-get update && sudo apt-get upgrade
 #install stuff
 sudo apt-get install xvfb firefox fdupes ssdeep python-pip unzip zip
 
-#download latest geckodriver
+#download geckodriver
 wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
 tar xzf geckodriver-v0.11.1-linux64.tar.gz
 rm geckodriver-v0.11.1-linux64.tar.gz
---
+++ slickshoes.sh
@@ -0,0 +1,25 @@
+nks out of pdf files
+#hadojae
+#param is path to pdf files
+#REQUIRES pdf-parser.py from https://blog.didierstevens.com/programs/pdf-tools/
+
+cd $1
+
+for i in *
+do
+
+file_type="$(file -b $i)"
+
+if [ "${file_type%%,*}" == "PDF document" ]; then
+    egrep -i -a -r -o --no-filename "http[^)]+" $i >> pdf_links.tmp
+    python ~/pdf/pdf-parser.py --searchstream --regex "https?:\/" --filter $i | egrep -i -a -o "http[^)\"\']+" >> pdf_links.tmp
+fi
+
+done
+
+#format up the file for printing
+
+sort -u pdf_links.tmp | egrep -i -v -a "(DidierStevens\.com|fonts\.com|typoland\.com|monotypeimaging\.com|monotype\.com|dynaforms\.com|youtube\.com|radpdf\.com|igouv\.fr|support\.microsoft\.com|maps\.google\.com|wikipedia\.org|mitre\.org|code\.google\.com|www\.w3\.org|purl\.org|adobe\.com|convertapi\.com|wondershare\.net|iec\.ch|verisign\.com|microsoft\.com|neevia\.com|aiim\.org|pdf\-tools\.com|color\.org)";
+
+#cleanup
+rm pdf_links.tmp
