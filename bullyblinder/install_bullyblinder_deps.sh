e3d15333697430579b533994deb24a3dd5d6e08d
hadojae <hadojae@users.noreply.github.com>
2017-06-28 23:14:35 -0500

    Update install_bullyblinder_deps.sh

--- bullyblinder/install_bullyblinder_deps.sh
+++ bullyblinder/install_bullyblinder_deps.sh
@@ -8,9 +8,9 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt-get -y install git xvfb python-pip firefox unzip zip tshark wireshark
 
 #download geckodriver
-wget https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz
-tar xzf geckodriver-v0.16.1-linux64.tar.gz
-rm geckodriver-v0.16.1-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-linux64.tar.gz
+tar xzf geckodriver-v0.17.0-linux64.tar.gz
+rm geckodriver-v0.17.0-linux64.tar.gz
 sudo chmod a+x geckodriver
 sudo mv geckodriver /usr/bin
 
